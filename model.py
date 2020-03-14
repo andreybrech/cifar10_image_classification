ee89721dff4d7fd7b57f7c6b7d0cd6ba71fbd788
andreybrech <51193190+andreybrech@users.noreply.github.com>
2020-03-14 14:10:20 +0300

    device changed to gpy, model changed

--- model.py
+++ model.py
@@ -6,8 +6,8 @@ class MLP(nn.Module):
     def __init__(self, input_size, num_classes):
         super(MLP,self).__init__()
         self.num_classes = num_classes
-        self.fc1 = nn.Linear(input_size,200)
-        self.fc2 = nn.Linear(200,200)
+        self.fc1 = nn.Linear(input_size,32*32)
+        self.fc2 = nn.Linear(32*32,200)
         self.fc3 = nn.Linear(200,num_classes)
         self.act = nn.ReLU()
 
--- train.py
+++ train.py
@@ -7,15 +7,18 @@ from model import MLP
 
 
 def main():
+
+    device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
     transform = transforms.Compose(
         [transforms.ToTensor(),
          transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
     data_train = datasets.CIFAR10('/data', train=True, download=True,transform=transform)
-    data_test = datasets.CIFAR10('/data', train=True, download=transform)
+    # data_test = datasets.CIFAR10('/data', train=True, download=transform)
     train_loader = DataLoader(data_train, num_workers=4, batch_size=32)
-    test_loader = DataLoader(data_test, num_workers=4, batch_size=16)
+    # test_loader = DataLoader(data_test, num_workers=4, batch_size=16)
 
     net = MLP(3*32*32,10)
+    net.to(device)
     print(net)
     criterion = nn.CrossEntropyLoss()
     optimizer = torch.optim.SGD(net.parameters(), lr=0.01)
@@ -26,16 +29,16 @@ def main():
         i = 0
         for data,labels in train_loader:
             i += 1
+            data,labels = data.to(device), labels.to(device)
             data = data.view(-1,3*32*32)
             optimizer.zero_grad()
             net_out = net(data)
             loss = criterion(net_out,labels)
-            # print(loss)
             loss.backward()
             optimizer.step()
 
             running_loss += loss.item()
-            if i % 100 == 0:  # print every 2000 mini-batches
+            if i % 100 == 0:  # print every 100 mini-batches
                 print('[%d, %5d] loss: %.3f' %
                       (epoch + 1, i + 1, running_loss / 100))
                 running_loss = 0.0
