7ef5f25e1a9870fee6c504aa797f8494222bec38
andreybrech <51193190+andreybrech@users.noreply.github.com>
2020-04-05 17:32:06 +0300

    added new parsing arguments
     - network_type
     - save_model
     - detailed_statistics

--- train.py
+++ train.py
@@ -16,12 +16,18 @@ parser.add_argument('--batch_size_test', type=int, default=16,
                     help="batch size for testing (default:16)")
 parser.add_argument('--epochs', type=int, default=5,
                     help="epochs number for training (default:5)")
-parser.add_argument('--lr', type=int, default=0.01,
+parser.add_argument('--lr', type=float, default=0.01,
                     help="learning rate (default:0.01)")
 parser.add_argument('--cuda', action='store_true', default=True,
                     help='enables CUDA training')
-parser.add_argument('--lr_sheduler', action='store_true', default=True,
+parser.add_argument('--lr_sheduler', action='store_true', default=False,
                     help='enables lr_sheduler training')
+parser.add_argument('--network_type', type=int, default=1,
+                    help="1: CNN; 2:MLP")
+parser.add_argument('--save_model', action='store_true', default=False,
+                    help='enables saving model')
+parser.add_argument('--detailed_statistics', action='store_true', default=False,
+                    help='enables detailed statistics: accuracy, avg_loss 4 time per epoch')
 
 
 def evaluate(model, test_loader, criterion, device):
@@ -45,7 +51,7 @@ def evaluate(model, test_loader, criterion, device):
     return avg_loss, acc
 
 
-def main(nn_type):
+def main():
     args = parser.parse_args()
     print(args)
     device = torch.device("cuda:0" if torch.cuda.is_available() and args.cuda else "cpu")
@@ -60,6 +66,10 @@ def main(nn_type):
     test_loader = DataLoader(data_test, num_workers=4, batch_size=args.batch_size_test)
 
     # configure network
+    if args.network_type == 1:
+        nn_type = CNN
+    elif args.network_type == 2:
+        nn_type = MLP
     net = nn_type(3 * 32 * 32, 10)
     net.to(device)
     print(net)
@@ -73,6 +83,7 @@ def main(nn_type):
 
     epochs = args.epochs
     running_loss = 0.0
+    info_every = int(len(train_loader) / 4)
     for epoch in range(epochs):
         net.train()
         for i, (data, labels) in enumerate(train_loader):
@@ -82,23 +93,26 @@ def main(nn_type):
             loss = criterion(net_out, labels)
             loss.backward()
             optimizer.step()
-
             running_loss += loss.item()
-            if i % 400 == 399:  # print every 400 mini-batches
-                print('[%d, %5d] loss: %.3f' %
-                      (epoch + 1, i + 1, running_loss / 400))
+            if i % info_every == info_every - 1:  # print every 400 mini-batches
+                print('[%d, %5d / %5d] loss: %.3f' %
+                      (epoch + 1, i + 1, len(train_loader), running_loss / info_every, ))
                 running_loss = 0.0
-                avg_loss, acc = evaluate(net, test_loader, criterion, device)
-                print(f'avg_loss: {avg_loss}, acc: {acc}')
-                writer.add_scalar('acc', acc, epoch * len(train_loader) + i)
-                writer.add_scalar('avg_loss', avg_loss, epoch * len(train_loader) + i)
-                writer.add_scalar('learning rate', scheduler.get_lr()[0], epoch * len(train_loader) + i)
-        scheduler.step()
-    print('Finished Training')
+                if args.detailed_statistics:
+                    avg_loss, acc = evaluate(net, test_loader, criterion, device)
+                    print(f'avg_loss: {avg_loss}, acc: {acc}')
+                    writer.add_scalar('acc', acc, epoch * len(train_loader) + i)
+                    writer.add_scalar('avg_loss', avg_loss, epoch * len(train_loader) + i)
+                    writer.add_scalar('learning rate', scheduler.get_lr()[0], epoch * len(train_loader) + i)
+        if args.lr_sheduler:
+            scheduler.step()
+    avg_loss, acc = evaluate(net, test_loader, criterion, device)
+    print(f'Finished Training. Avarage loss: {avg_loss}, accuracy: {acc}')
     # save model
-    PATH = './cifar_net.pth'
-    # torch.save(net.state_dict(), PATH)
+    if args.save_model:
+        PATH = './cifar_net.pth'
+        torch.save(net.state_dict(), PATH)
 
 
 if __name__ == '__main__':
-    main(MLP)
+    main()
